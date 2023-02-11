b6f67ad71e05777c5dcc9cbece406f6b91241937
OgnyanY <o.yonchev@abv.bg>
2023-02-12 01:12:33 +0200

    add login page and log out functionality

--- my_movie_list/my_movie_list/settings.py
+++ my_movie_list/my_movie_list/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "gui/static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login'
