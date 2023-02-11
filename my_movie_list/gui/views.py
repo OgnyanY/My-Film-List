c392b61bc9f84e397b2192e02a656538e2cd4a6b
OgnyanY <o.yonchev@abv.bg>
2023-02-12 01:13:13 +0200

    add base and home page templates

--- my_movie_list/gui/views.py
+++ my_movie_list/gui/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/login')
 def index(request):
     """Welcome page."""
-    return HttpResponse('Welcome!')
+    return render(request, 'home_page.html')
