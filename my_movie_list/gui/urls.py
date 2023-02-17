9372a21be27792be7f41819fe198cbd6bb5876ea
OgnyanY <o.yonchev@abv.bg>
2023-02-17 12:28:56 +0200

    add login and register functionality

--- my_movie_list/gui/urls.py
+++ my_movie_list/gui/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('login', auth_views.LoginView.as_view()),
-    path('logout', auth_views.LogoutView.as_view())
+
+    path('', auth_views.LoginView.as_view(
+        template_name='registration/login.html'), name='login'),
+    path('movie_list/', views.movie_list, name='movie_list'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+
+    
 ]
--- my_movie_list/gui/views.py
+++ my_movie_list/gui/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
+from .forms import MovieListForm, MovieForm
+from .models import MovieList, Movie
 
-@login_required(login_url='/login')
-def index(request):
-    """Welcome page."""
-    return render(request, 'home_page.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('movie_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+@login_required
+def movie_list(request):
+    movie_lists = MovieList.objects.all()
+    return render(request, 'movie_list.html', {'movie_lists': movie_lists})
