bdb4a0c8b3aa849731e3ed861f20cc9ecee9fc7e
OgnyanY <o.yonchev@abv.bg>
2023-02-18 02:36:06 +0200

    format the code

--- my_movie_list/gui/models.py
+++ my_movie_list/gui/models.py
@@ -19,7 +19,8 @@ class Series(models.Model):
     rating = models.DecimalField(max_digits=3, decimal_places=1)
     year_release = models.PositiveSmallIntegerField(
         default=0, null=True, blank=True)
-    year_completion = models.PositiveSmallIntegerField(null=True, blank=True)
+    year_completion = models.PositiveSmallIntegerField(
+        null=True, blank=True)
     STATUS_CHOICES = [
         ('O', 'On going'),
         ('C', 'Completed'),
@@ -34,10 +35,12 @@ class Series(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
-    movies_watched = models.ManyToManyField(Movie, related_name='watched_by')
+    movies_watched = models.ManyToManyField(
+        Movie, related_name='watched_by')
     movies_plan_to_watch = models.ManyToManyField(
         Movie, related_name='plan_to_watch_by')
-    series_watched = models.ManyToManyField(Series, related_name='watched_by')
+    series_watched = models.ManyToManyField(
+        Series, related_name='watched_by')
     series_plan_to_watch = models.ManyToManyField(
         Series, related_name='plan_to_watch_by')
 
